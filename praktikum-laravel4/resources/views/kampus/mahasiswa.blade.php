c83f2d0b0a8ebd2c6fd27230942a33b0f1223442
RahmatSiddiqi <rahmatsiddiq946@gmail.com>
2022-03-16 15:12:34 +0700

    komit prak 4

---
+++ praktikum-laravel4/resources/views/kampus/mahasiswa.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/boostrap.min.css" >
+    <title>Data mahasiswa</title>
+</head>
+<body>
+    <div class="container text-centermt-3pt-3bg-white">
+        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">
+            <?php echo $nama?>
+        </h1>
+        <h1 class="bg-dark px-3 py-1 text-white d-inline-block">
+            <?php echo $nilai?>
+        </h1>
+    </div>
+</body>
+</html>

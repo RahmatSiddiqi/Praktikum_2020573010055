725844de28db5c0557a56cb93dcaa3b0ae5dd7ff
RahmatSiddiqi <rahmatsiddiq946@gmail.com>
2022-03-28 23:46:29 +0700

    commit prak 5 jobsheet 4

---
+++ praktikum-laravel5/resources/views/mahasiswa.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.master')
+@section('title','Data Mahasiswa')
+@section('menumahasiswa','active')
+@section('content')
+@parent
+    <div class="container text-center mt-3 p-4 bg-white">
+        <h1 class="mb-3">Data mahasiswa</h1>
+        <div class="row">
+            <div class="col-sm-8 col-md-6 m-auto">
+                <ol class="list-group">
+                    @forelse($mahasiswa as $val)
+                    <li class="list-group-item">{{$val}}</li>
+                    @empty
+                    <div class="alert alert-dark d-inline-block">tidak ada data</div>
+                    @endforelse
+                </ol>
+            </div>
+        </div>
+    </div>
+
+@endsection
+<p>sebuah paragraf</p>
